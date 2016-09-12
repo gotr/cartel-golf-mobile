ad726046c758ff1d8ef981328d859ac9ea8b5ce3
jim-clark <email@jim-clark.com>
2016-09-12 14:03:05 -0700

    Update signup view

--- app/pages/signup-invite/signup-invite.ts
+++ app/pages/signup-invite/signup-invite.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms'
+
+import {RealtimeDataService} from '../../providers/realtime-data-service/realtime-data-service';
 
 /*
   Generated class for the SignupInvitePage page.
@@ -8,12 +11,20 @@ import { NavController } from 'ionic-angular';
   Ionic pages and navigation.
 */
 @Component({
-  templateUrl: 'build/pages/signup-invite/signup-invite.html',
+  templateUrl: 'build/pages/signup-invite/signup-invite.html'
 })
 export class SignupInvitePage {
 
-  constructor(private navCtrl: NavController) {
-    
+  tokenForm: FormGroup;
+
+  constructor(private navCtrl: NavController, private formBuilder: FormBuilder) {
+    this.tokenForm = formBuilder.group({  
+      'tokenInput': ['', Validators.compose([Validators.required, Validators.minLength(16), Validators.maxLength(16)])]
+    });
+  }
+
+  onSubmit(formData) {
+    console.log(formData);
   }
 
 }
